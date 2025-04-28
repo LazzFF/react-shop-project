a8e2eab90cef145cb06ab5912769a886d041e222
Ksenia <pogorelovaxenia@yandex.ru>
2025-04-28 17:09:53 +0300

    first commit

---
+++ react-shop/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header, Shop, Footer, } from "./layout";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Shop />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ react-shop/src/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'e0aadb4c-2012e98c-ddb84bbc-e2ab4bcb';
+export const API_URL = 'https://fortniteapi.io/v2/shop?lang=en'
